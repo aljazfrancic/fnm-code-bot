7b4237f36f4aecf14c051ef6cb525f1012031a85
aljazfrancic <aljaz.francic@gmail.com>
2021-01-27 22:59:54 +0100

    el progresso classico

--- bot.py
+++ bot.py
@@ -9,6 +9,22 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(str(client.user) + " reporting for duty!")
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+        
+    command = "!fnm-code-bot-deploy"
+    
+    if message.content[:len(command)] == command:
+        await message.channel.send("Roger, roger!")
+        
+        input = message.content.split()
+        
+        channel = input[1]
+        fnmcodes = input[2:]
+        print(channel, fnmcodes)
+        
 client.run(TOKEN)
